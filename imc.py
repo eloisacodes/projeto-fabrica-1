219c756fa834141dffd939f7730d28a2dcaafec1
eloisacodes <eloisa.codes@gmail.com>
2025-10-29 10:49:32 -0300

    Create imc.py

---
+++ imc.py
@@ -0,0 +1,15 @@
+nota1 = float(input("Digite a 1ª nota: "))
+nota2 = float(input("Digite a 2ª nota: "))
+nota3 = float(input("Digite a 3ª nota: "))
+nota4 = float(input("Digite a 4ª nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f"\nMédia: {media:.2f}")
+
+if media >= 7.0:
+    print("Situação: Aprovado(a) ✅")
+elif media >= 5.0:
+    print("Situação: Recuperação ⚠️")
+else:
+    print("Situação: Reprovado(a) ❌")
